5b20f7aa7e95ee612dd5de560cbec1befc36ec1d
selimon <dida.z876@gmail.com>
2019-02-04 17:16:15 +0200

    add

--- js/myscript.js
+++ js/myscript.js
@@ -1,2 +1,7 @@
-var x = 1;
-let studentName = "selomon";
+
+jQuery(function(f){
+    var element = f('#back-top');
+    f(window).scroll(function(){
+        element['fade'+ (f(this).scrollTop() > 400 ? 'In': 'Out')](200);
+    });
+});
