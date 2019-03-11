4fb7f7d9f7aa9c1c7f91de9f5aa3ca3154f0eb93
selimon <dida.z876@gmail.com>
2019-03-11 17:21:06 +0200

    sssa

--- js/slow_scroll.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	function slowScroll(id){ // id элемента, нажав на который будет работать анимация
-		var offset = 50;
-		$('html, body').animate({
-			scrollTop: $(id).offset().top - offset
-		}, 500);
-		return false;
-	}
-});
